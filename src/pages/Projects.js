aa422966d2b663fa4b5dd036543548c46b527660
nabiladigo <nabiladigo91@gmail.com>
2022-03-01 22:55:22 -0500

    update frontend

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,14 +15,14 @@ function Projects(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getProjectsData(), []);
+  useEffect(() => getProjectsData());
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
     return projects.map((project) => (
       <div>
         <h1>{project.name}</h1>
-        <img src={project.image} />
+        <img src={project.image} alt={project.name}/>
         <a href={project.git}>
           <button>Github</button>
         </a>
