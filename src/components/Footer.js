9beb3e4503975bfaa8374e5513da4164f0a4749a
nabiladigo <nabiladigo91@gmail.com>
2022-03-02 00:43:36 -0500

    update frontend

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,19 @@
-function Footer(props) {
-    return <h1>Footer</h1>;
-  }
-  
+import {useEffect, useState} from "react-router-dom";
+function Footer(props){
+  const [footer, setFooter]= useState(null);
+  const getFooterData = async() =>{
+    const response = await fetch(props.URL + "footer");
+    const data = await response.json();
+    setFooter(data);
+  };
+  useEffect(()=> getFooterData());
+  const loaded = () => (
+    <div>
+      <div>Designed & Build by Nabila Digourdi </div>
+    </div>
+  );
+  return footer ? loaded() : <h1>loading...</h1>
+};
+
+ 
   export default Footer;
